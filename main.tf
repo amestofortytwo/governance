0364e182d84d4671caef30ae38fba364a6715955
Roberth Strand <roberth.strand@amesto.no>
2023-01-19 16:29:33 +0100

    feat: added logic for users and teams
    To make it easier to read, I created two resources for users, one for admins and one for regular users. I also added a team for all users, and a membership loop that takes all users and adds them to the group.
    
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- main.tf
+++ main.tf
@@ -4,8 +4,25 @@
 * This is a repository for managing our GitHub organization. Anyone from Fortytwo can add members through a pull request, and any member can approve it.
 */
 
-resource "github_membership" "main" {
-  for_each = { for u in local.users : u.username => u }
+resource "github_membership" "admin" {
+  for_each = local.admins
   username = each.key
-  role     = each.value.role
+  role     = "admin"
+}
+
+resource "github_membership" "users" {
+  for_each = local.users
+  username = each.key
+}
+
+resource "github_team" "all_users" {
+  name        = "All Users"
+  description = "Every user in the organization."
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "all_users" {
+  for_each = setunion(local.users, local.admins)
+  username = each.key
+  team_id  = github_team.all_users.id
 }
--- users.tf
+++ users.tf
@@ -1,8 +1,24 @@
 locals {
-  users = [
-    {
-      username = "roberthstrand"
-      role     = "admin"
-    }
-  ]
+  admins = toset([
+    "4t2-marvin",
+    "roberthstrand",
+    "mariussm",
+    "olebru",
+    "remiks",
+    "sGrindvoll",
+    "sikksakk"
+  ])
+
+  users = toset([
+    "alegri90",
+    "bittib010",
+    "dahol",
+    "eigard",
+    "FRGulli",
+    "hannelan",
+    "kjehanse",
+    "timpeteren",
+    "warfair1337",
+    "weq"
+  ])
 }
