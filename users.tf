e3c6a9af04a7c50abac5f6f9d86138064aec0c14
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-05-02 10:47:16 +0200

    added comments for all users with their actual names

--- users.tf
+++ users.tf
@@ -1,26 +1,26 @@
 locals {
   admins = toset([
-    "4t2-marvin",
-    "roberthstrand",
-    "mariussm",
-    "olebru",
-    "remiks",
-    "sGrindvoll",
-    "sikksakk"
+    "4t2-marvin",    # Marvin
+    "roberthstrand", # Roberth Strand
+    "mariussm",      # Marius Solbakken
+    "olebru",        # Ole Idar Bruvik
+    "remiks",        # Remi Vandemir
+    "sGrindvoll",    # Sindre Grindvoll
+    "sikksakk"       # Christoffer Thomsen
   ])
 
   users = toset([
     "alegri90",
     "bittib010",
     "dahol",
-    "eigard",
-    "FRGulli",
-    "hannelan",
-    "kjehanse",
-    "timpeteren",
+    "eigard",     # Morten Eigard
+    "FRGulli",    # Frank Gulli
+    "hannelan",   # Hannelen Leirvåg
+    "kjehanse",   # Kjetil Hansen
+    "timpeteren", # Tim Peter Edstrøm
     "warfair1337",
     "weq",
-    "benaarvik",
-    "mysteq"
+    "benaarvik", # Bendik Aarvik
+    "mysteq"     # Ketil Gjerde
   ])
 }
