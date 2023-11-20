26d2c0028f0cb2f3b47ac451828f8dac6d1f2de7
Ketil Gjerde <ketil.gjerde@amesto.no>
2023-11-20 10:22:08 +0100

    style: fix formatting
    Signed-off-by: Ketil Gjerde <ketil.gjerde@amesto.no>

--- users.tf
+++ users.tf
@@ -13,18 +13,18 @@ locals {
   ])
 
   users = toset([
-    "antnsn",    # Marius Antonsen
-    "alegri90",  # Alexander Grimstad
-    "bittib010", # Adrian Kyland Skaftun
-    "dcstrand",  # Dominique Cid-Strand
-    "eigard",    # Morten Eigard
-    "FRGulli",   # Frank Gulli
-    "hannelan",  # Hannelen Leirvåg
-    "kjehanse",  # Kjetil Hansen
-    "timpeteren", # Tim Peter Edstrøm
-    "weq",        # Raymond Siring
-    "benaarvik",  # Bendik Aarvik
-    "andrenascentes"  # Andrè Nascentes
+    "antnsn",        # Marius Antonsen
+    "alegri90",      # Alexander Grimstad
+    "bittib010",     # Adrian Kyland Skaftun
+    "dcstrand",      # Dominique Cid-Strand
+    "eigard",        # Morten Eigard
+    "FRGulli",       # Frank Gulli
+    "hannelan",      # Hannelen Leirvåg
+    "kjehanse",      # Kjetil Hansen
+    "timpeteren",    # Tim Peter Edstrøm
+    "weq",           # Raymond Siring
+    "benaarvik",     # Bendik Aarvik
+    "andrenascentes" # Andrè Nascentes
   ])
   /*
   outside_collaborators = { # Not used, purely for documentation purposes
